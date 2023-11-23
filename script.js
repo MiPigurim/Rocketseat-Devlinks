8fbdaf285db6541d3b6df206993a4a25d4e5954d
MiPigurim <michelepigurim26@gmail.com>
2023-11-23 14:34:12 -0300

    feat: alteração de imagem no mode

--- script.js
+++ script.js
@@ -8,4 +8,23 @@ function toggleMode() {
   //  }
 
   html.classList.toggle("light"); //o toggle é uma função quejá aplica semelhante à lógica montada a cima
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img");
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute(
+      "alt",
+      "Foto de Maky Brito sorrindo, usando óculos escuros e jaqueta preta em fundo azul"
+    );
+  } else {
+    //sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute(
+      "alt",
+      "Foto de Maky Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo"
+    );
+  }
 }
