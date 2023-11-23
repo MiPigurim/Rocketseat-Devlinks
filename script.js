66096edb7c0121b90d1bf527ea068097e4b699a6
MiPigurim <michelepigurim26@gmail.com>
2023-11-23 14:17:55 -0300

    feat: add script.js inteligência alteração de mode

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement;
+
+  //  if (html.classList.contains("light")) {
+  //    html.classList.remove("light");
+  //  } else {
+  //    html.classList.add("light");
+  //  }
+
+  html.classList.toggle("light"); //o toggle é uma função quejá aplica semelhante à lógica montada a cima
+}
